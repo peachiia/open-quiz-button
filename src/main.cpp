ef0f8d8d3f778a17964ed3a58bb4475656b6adab
Peachiia <pk.destinazione@gmail.com>
2018-05-25 23:39:06 +0700

    Add BoxIO task

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,15 @@
 #define STATIC_TIMER_INIT static unsigned long timer = millis();  // Schedule Variable Declaration
 #define STATIC_TIMER_CHECK (millis() >= timer)                    // Schedule Checking
 
+#define TEAMIO_TASK_PERIOD 5
+void init_teamio();
+void task_teamio_update(int period);
+
+#define BOXIO_TASK_PERIOD 50
+void init_boxio();
+void task_boxio_update(int period);
+
+
 #define TEAM_MAX 6
 int teamInput_threshold = 512;
 
@@ -12,34 +21,45 @@ int teamInput_uniqueActiveID = 0;
 
 bool teamInput_isActive[TEAM_MAX];
 bool teamOutput_isActive[TEAM_MAX];
-int teamInput_Pin[] = {A0, A1, A2, A3, A4, A5, A6};
+int teamInput_Pin[] = {A0, A1, A2, A3, A4, A5};
 int teamOutput_Pin[] = {3, 4, 5, 6, 7, 8};
 
 
 
-bool boxButton_isActive;
+
+bool boxButton_isActive; // Current State
+bool boxButton_isHold;   // Holding State
+
+bool boxButton_longPressedFlag;             // Raised if hold > threshold
+bool boxButton_longPressedCounter = 0;
+bool boxButton_longPressedThreshold = 2000 / BOXIO_TASK_PERIOD; // 2000 ms
+
 bool boxBuzzer_isActive;
 bool boxLed_isActive;
 
-void init_teamio();
-void task_teamio_update(int period);
+int boxButton_Pin = 0;
+int boxBuzzer_Pin = 0;
+int boxLed_Pin = 0;
 
 
 
 void setup() 
 {
+    init_boxio();
     init_teamio();
 }
 
 void loop() 
 { 
     task_teamio_update(5);
+    task_boxio_update(50);
 }
 
 
 
 void init_teamio()
 {
+    // Inputs don't have to init because it's analogPin
     for (int i = 0; i < TEAM_MAX; i++) {
         pinMode(teamOutput_Pin[i], OUTPUT);
     }
@@ -58,12 +78,14 @@ void task_teamio_update(int period)
             teamInput_isActive[i] = analogRead(teamInput_Pin[i]) > teamInput_threshold;
             if (teamInput_isActive[i]) 
             {
-                if (activeFlag == false && activeID == -1) {
+                if (activeFlag == false) {
                     activeFlag = true;
                     activeID = i;
                 }
                 else {
-                    activeFlag = false;
+                    if (random(99) % 2) {
+                        activeID = i;
+                    }
                 }
             }
         }
@@ -73,6 +95,36 @@ void task_teamio_update(int period)
             teamInput_uniqueActiveID = activeID;
         }
 
+        STATIC_TIMER_UPDATE;
+    }
+}
+
+
+void init_boxio()
+{
+    pinMode(boxButton_Pin, INPUT);
+    pinMode(boxBuzzer_Pin, OUTPUT);
+    pinMode(boxLed_Pin, OUTPUT);
+}
+
+
+void task_boxio_update(int period)
+{
+    STATIC_TIMER_INIT;
+    if (STATIC_TIMER_CHECK)
+    {
+        boxButton_isActive = digitalRead(boxButton_Pin);
+        if (boxButton_isActive) {
+            boxButton_longPressedCounter++;
+            if (boxButton_longPressedCounter >= boxButton_longPressedThreshold) {
+                boxButton_longPressedFlag = true;
+            }
+        }
+        else {
+            boxButton_longPressedCounter = 0;
+        }
+        digitalWrite(boxLed_Pin, boxLed_isActive);
+        digitalWrite(boxBuzzer_Pin, boxBuzzer_isActive);
         STATIC_TIMER_UPDATE;
     }
 }
